2eb0fed082f9de15e1c2c7cdbfb84fd8727c04cd
Maxtone01 <originsthe@gmail.com>
2023-03-01 16:36:11 +0200

    implement tooltip window to quests

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -17,15 +17,15 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Collectable.Instance.quantity == 4)
-        { 
-            startPlayerChase = true;
-        }
-        if (Collectable.Instance.quantity == 5)
-        {
-            startPlayerChase = false;
-            ResetPath();
-        }
+        //if (Collectable.Instance.quantity == 4)
+        //{ 
+        //    startPlayerChase = true;
+        //}
+        //if (Collectable.Instance.quantity == 5)
+        //{
+        //    startPlayerChase = false;
+        //    ResetPath();
+        //}
     }
 
     private void ResetPath()
